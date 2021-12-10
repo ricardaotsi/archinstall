73335458eb822b12ef09a3505fcdf7a7ad64bc42
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2021-12-10 15:50:59 -0300

    wibar

--- .config/awesome/themes/zenburn/theme.lua
+++ .config/awesome/themes/zenburn/theme.lua
@@ -15,10 +15,10 @@ theme.wallpaper = themes_path .. "zenburn/zenburn-background.jpg"
 theme.font      = "sans 8"
 
 -- {{{ Colors
-theme.fg_normal  = "#DCDCCC"
+theme.fg_normal  = "#312938"
 theme.fg_focus   = "#F0DFAF"
 theme.fg_urgent  = "#CC9393"
-theme.bg_normal  = "#3F3F3F"
+theme.bg_normal  = "#312938"
 theme.bg_focus   = "#1E2320"
 theme.bg_urgent  = "#3F3F3F"
 theme.bg_systray = theme.bg_normal
