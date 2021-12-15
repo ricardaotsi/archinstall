0f813a8c95cbcf11d5c87ff91144ce62b73ec97c
Ricardo Arruda <ricardo.sowek@gmail.com>
2021-12-14 21:19:11 -0300

    .

--- .config/awesome/themes/zenburn/theme.lua
+++ .config/awesome/themes/zenburn/theme.lua
@@ -26,9 +26,9 @@ theme.bg_systray = theme.bg_normal
 
 -- {{{ Borders
 theme.useless_gap   = dpi(6)
-theme.border_width  = dpi(0)
+theme.border_width  = dpi(2)
 theme.border_normal = "#3F3F3F"
-theme.border_focus  = "#6F6F6F"
+theme.border_focus  = "#ffffff"
 theme.border_marked = "#CC9393"
 -- }}}
 
