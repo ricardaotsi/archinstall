f2c69b1ad73064b73be0b897f7fb614d0d004f30
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2021-12-29 10:12:25 -0300

    post install

---
+++ postInstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo pacman -S --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd $HOME
+yay -Y --gendb
+yay -Y --devel --save
+rm -rf yay-bin
+git clone https://github.com/ricardaotsi/qtile-nord-dotfiles.git
+cd qtile-nord-dotfiles
+pacman -S --noconfirm $(awk '{print $1}' requirements)
+yay -S picom-jonaburg-git nerd-fonts-hack
+yes | pip install psutil
+chsh -s /bin/fish
+cp .xinitrc $HOME
+cp -r .config $HOME
+cd $HOME
+rm -rf qtile-nord-dotfiles
+printf "Install video drivers and reboot"
+
